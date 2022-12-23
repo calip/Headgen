e1e7ec00227957a04bb9f039288dbc6864fad2be
alip putra <asalip.putra@gmail.com>
2022-12-23 15:11:19 +0700

    json file upload dialog component

--- src/components/Dialog/File.js
+++ src/components/Dialog/File.js
@@ -1,18 +1,39 @@
-import { Button, Modal } from 'react-bootstrap'
+import { useState } from 'react'
+import { Button, FormControl, FormGroup, Modal } from 'react-bootstrap'
 
-function File({ show, dialogFn, actionFn }) {
+function File({ show, dialogFn, actionFn, setJson }) {
+  const [disable, setDisable] = useState(false)
+  const handleFileChange = (e) => {
+    const file = e.target.files[0]
+    if (file.type != 'application/json') {
+      alert('Only Json are valid.')
+      setDisable(true)
+      return
+    }
+    setDisable(false)
+    const fileReader = new FileReader()
+    fileReader.readAsText(e.target.files[0], 'UTF-8')
+    fileReader.onload = (e) => {
+      setJson(e.target.result)
+    }
+  }
+
   return (
     <>
       <Modal show={show} onHide={dialogFn}>
         <Modal.Header closeButton>
           <Modal.Title>Load JSON file</Modal.Title>
         </Modal.Header>
-        <Modal.Body>load file from json?</Modal.Body>
+        <Modal.Body>
+          <FormGroup controlId="jsonFile" className="mb-3">
+            <FormControl type="file" onChange={handleFileChange} />
+          </FormGroup>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={dialogFn}>
             Cancel
           </Button>
-          <Button variant="primary" onClick={actionFn}>
+          <Button variant="primary" onClick={actionFn} disabled={disable}>
             Load
           </Button>
         </Modal.Footer>
