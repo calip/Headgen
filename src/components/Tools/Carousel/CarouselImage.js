d36b9562cdd0c75feb74da6fa7b716a27824b69a
alip putra <asalip.putra@gmail.com>
2023-04-19 14:08:19 +0700

    add carousel image component

--- src/components/Tools/Carousel/CarouselImage.js
+++ src/components/Tools/Carousel/CarouselImage.js
@@ -1,14 +1,22 @@
-import { Carousel, Col, Container, Row } from 'react-bootstrap'
+import { Button, Carousel, Col, Container, Row } from 'react-bootstrap'
 import './CarouselImage.scss'
+import { useState } from 'react'
 
 function CarouselImage(props) {
+  const [activeSize, setActiveSize] = useState()
+
+  const handleSelectSize = (size) => {
+    setActiveSize(size)
+    props.onSelectSize(size)
+  }
+
   return (
     <Container fluid>
       <Row>
         <Col>
           <div className="carousel-content">
             <Carousel variant="dark">
-              {props.images.map((item, index) => {
+              {props.format.images.map((item, index) => {
                 return (
                   <Carousel.Item key={index}>
                     <img className="d-block w-100" src={item} alt="carousel image" />
@@ -16,6 +24,20 @@ function CarouselImage(props) {
                 )
               })}
             </Carousel>
+          </div>
+          <div className="carousel-option">
+            {props.format.sizes.map((size, index) => {
+              return (
+                <Button
+                  variant="outline-dark"
+                  active={index === activeSize}
+                  size="sm"
+                  key={index}
+                  onClick={() => handleSelectSize(index)}>
+                  {size.width}x{size.height}
+                </Button>
+              )
+            })}
           </div>
         </Col>
       </Row>
