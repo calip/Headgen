1f38c79475023c62eaf6b5c786657edc069bd0fa
alip putra <asalip.putra@gmail.com>
2023-05-26 10:31:16 +0700

    add selected product and variation from woocommere

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App() {
   const [config, setConfig] = useState({})
   const [loading, setLoading] = useState(true)
   const [products, setProducts] = useState([])
+  const [selectedProduct, setSelectedProduct] = useState()
+  const [selectedVariation, setSelectedVariation] = useState()
 
   const fontComfortaa = new FontFaceObserver('Comfortaa-Wordpix')
   const fontItalianno = new FontFaceObserver('Italianno-Wordpix')
@@ -19,40 +21,97 @@ function App() {
   const fontMexcellent = new FontFaceObserver('Mexcellent-Wordpix')
   const fontStripey = new FontFaceObserver('Stripey-Wordpix')
 
+  const selectItem = {
+    selectedProduct: selectedProduct,
+    setSelectedProduct: setSelectedProduct,
+    selectedVariation: selectedVariation,
+    setSelectedVariation: setSelectedVariation
+  }
+
+  const timeout = (time) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(reject, time)
+    })
+  }
+
   useEffect(() => {
     setLoading(true)
-    Helpers.fetchJson(`${process.env.REACT_APP_BASE}/config/config.json`).then((result) => {
-      setConfig(result)
-      Promise.all([
-        fontComfortaa.load(null, 9000),
-        fontItalianno.load(null, 9000),
-        fontMarqueeMoon.load(null, 9000),
-        fontMexcellent.load(null, 9000),
-        fontStripey.load(null, 9000)
-      ]).then(function () {
-        const api = Woocommerce(result)
-        api
-          .get('products', {
-            per_page: 20,
-            type: 'variable',
-            status: 'publish'
-          })
-          .then((response) => {
-            if (response.status === 200) {
-              setProducts(response.data)
-              setLoading(false)
-            }
-          })
-          .catch((error) => {
-            console.log(error)
-          })
+    Promise.race([
+      fontComfortaa.load(),
+      fontItalianno.load(),
+      fontMarqueeMoon.load(),
+      fontMexcellent.load(),
+      fontStripey.load(),
+      timeout(3000)
+    ]).then(() => {
+      Helpers.fetchJson(`${process.env.REACT_APP_BASE}/config/config.json`).then((result) => {
+        setConfig(result)
+        if (result.wordpress.active) {
+          const api = Woocommerce(result)
+          api
+            .get('products', {
+              per_page: 20,
+              type: 'variable',
+              status: 'publish'
+            })
+            .then((response) => {
+              if (response.status === 200) {
+                const search = window.location.search
+                const params = new URLSearchParams(search)
+                const editor = params.get('editor')
+                if (editor) {
+                  const param = Helpers.decodeJsonData(editor)
+                  api
+                    .get('products/' + param.product_id, {})
+                    .then((resproduct) => {
+                      if (resproduct.status === 200) {
+                        const product = Helpers.extractProduct(resproduct.data)
+                        api
+                          .get(
+                            'products/' + param.product_id + '/variations/' + param.variation_id,
+                            {}
+                          )
+                          .then((res) => {
+                            if (res.status === 200) {
+                              const variation = Helpers.extractVariation(res.data)
+                              setSelectedProduct(product)
+                              setSelectedVariation(variation)
+                              setProducts(response.data)
+                              setLoading(false)
+                              Helpers.clearUrlHistory(result)
+                            }
+                          })
+                          .catch((error) => {
+                            console.log(error)
+                          })
+                      }
+                    })
+                    .catch((error) => {
+                      console.log(error)
+                    })
+                } else {
+                  setProducts(response.data)
+                  setLoading(false)
+                }
+              }
+            })
+            .catch((error) => {
+              console.log(error)
+            })
+        } else {
+          setLoading(false)
+        }
       })
     })
   }, [])
 
   return (
     <div className="App">
-      {loading ? <Loader /> : <Editor config={config} products={products} />}
+      {loading ? (
+        <Loader />
+      ) : (
+        <Editor config={config} products={products} selectItem={selectItem} />
+      )}
     </div>
   )
 }
