e73e6e60d39f2ecd45fbaa19f1ffefe283318058
alip putra <asalip.putra@gmail.com>
2023-06-26 14:13:33 +0700

    add custom description tooltip button

--- src/components/Tools/Tooltips/Tooltips.js
+++ src/components/Tools/Tooltips/Tooltips.js
@@ -3,7 +3,7 @@ import { Overlay, Tooltip } from 'react-bootstrap'
 function Tooltips(props) {
   return (
     <Overlay target={props.target} show={props.show} placement={props.position}>
-      <Tooltip id="overlay-example">{props.description}</Tooltip>
+      <Tooltip>{props.description}</Tooltip>
     </Overlay>
   )
 }
