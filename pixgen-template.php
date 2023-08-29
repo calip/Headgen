3695e1992719200325759ff7373867720ff3d72a
alip putra <asalip.putra@gmail.com>
2023-08-29 12:50:05 +0700

    add wordpress container plugin

---
+++ pixgen-template.php
@@ -0,0 +1,13 @@
+<?php
+ /**
+   * Template name: pixgen-editor
+   */
+
+get_header(); ?>
+  
+<div id="primary" class="content-area">
+	<?php echo do_shortcode("[pixgen_editor]"); ?>
+</div>
+
+<?php
+get_footer(); ?>
